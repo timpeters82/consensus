1bdd50c9416bfb9a6648deb3b75082390938710b
Tim Peters <t.peters@garvan.org.au>
2018-06-12 14:18:49 +1000

    consensus_0.99.9

--- R/show-Methods.R
+++ R/show-Methods.R
@@ -2,12 +2,12 @@ setMethod("show", "MultiMeasure",
           function (object) cat(paste0("MultiMeasure object with ",
                                        length(object@data), " platforms/conditions, ",
                                        ncol(object@data[[1]]), " samples and ", 
-                                       nrow(object@data[[1]]), " measured loci."))
+                                       nrow(object@data[[1]]), " measured loci.\n"))
 )
 
 setMethod("show", "ConsensusFit",
           function (object) cat(paste0("ConsensusFit object with ",
                                        ncol(object@a_i), " platforms/conditions and ",
-                                       nrow(object@a_i), " measured loci."))
+                                       nrow(object@a_i), " measured loci.\n"))
 )
 
